480b39332c47f7fda950b3015f5fdd68919d3644
Tyler Stright <tylers@nezperce.org>
2023-08-16 11:27:04 -0600

    building out home page, updating css

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { AuthService } from 'src/_services/auth.service';
-import { User } from 'src/_models/user';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,17 +10,10 @@ export class HomeComponent implements OnInit {
   token: any | null = null;
 
   constructor(
-    private authService: AuthService,
   ) { }
 
   ngOnInit() {
-    this.authService.token$.subscribe((token) => {
-      this.token = token;
-    });
-  }
 
-  getTokenVal() {
-    console.log(this.authService.token$.getValue());
   }
 
 }
