39c2e4014d8cfcf4731d232453c72b61b86532d7
Tyler Stright <tylers@nezperce.org>
2023-08-24 10:27:55 -0600

    profile now hiding button if not current user.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ActivatedRoute } from '@angular/router';
 
 import { BackendService } from 'src/_services/backend.service';
 import { User } from 'src/_models/user';
 
 import { ProfileUpdateComponent } from '../forms/profile-update/profile-update.component';
+import { getRouteType, getRouteSlug } from 'src/_utilities/route-utils';
+
 
 @Component({
   selector: 'app-profile',
@@ -15,21 +18,36 @@ export class ProfileComponent implements OnInit {
 
   user: User | undefined;
   imageUrl!: string | null;
+  routeType!: string;
 
   constructor(
     private bs: BackendService,
+    private route: ActivatedRoute,
     public modalService: NgbModal,
-  ) {  }
+  ) { }
 
   ngOnInit() {
-    this.bs.getCurrentUser().subscribe((user: any) => {
-      this.user = user;
-      this.getImage(user.profile.photo);
-    });
+    this.getUserData();
+  }
+
+  getUserData() {
+    // profile component is for user info and also user profile.
+    this.routeType = getRouteType(this.route);
+
+    if (this.routeType === 'users') {
+      const slug = getRouteSlug(this.route);  // actually 'id'
+      this.bs.getDetail(this.routeType, slug).subscribe(user => {
+        this.user = user;
+      });
+    } else {
+      this.bs.getCurrentUser().subscribe(currentUser => {
+        this.user = currentUser;
+      });
+    }
   }
 
   updateProfile() {
-    const modalRef = this.modalService.open(ProfileUpdateComponent, { size: 'xl'});
+    const modalRef = this.modalService.open(ProfileUpdateComponent, { size: 'xl' });
     modalRef.componentInstance.user = this.user; // pass user to modal
 
     modalRef.componentInstance.userUpdated.subscribe((updatedUser: any) => {
@@ -38,7 +56,7 @@ export class ProfileComponent implements OnInit {
   }
 
   getImage(path: string) {
-    this.imageUrl = path.replace('http://localhost:4200', 'http://localhost:8000' );  
+    this.imageUrl = path.replace('http://localhost:4200', 'http://localhost:8000');
   }
 
 }
