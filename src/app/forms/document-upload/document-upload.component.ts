8d08c827460685f4374be6f13b2915091eb1fcb0
Tyler Stright <tylers@nezperce.org>
2024-12-10 16:37:58 -0700

    updating functionality of file input and doc upload. Almost working

--- src/app/forms/document-upload/document-upload.component.ts
+++ src/app/forms/document-upload/document-upload.component.ts
@@ -1,14 +1,12 @@
 import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { AlertService } from 'src/_services/alert.service';
-import { BackendService } from 'src/_services/backend.service';
-import { AuthService } from 'src/_services/auth.service';
+import { Subscription } from 'rxjs';
 
 import { DocumentsComponent } from '../../documents/documents.component';
 import { UserService } from 'src/_services/user.service';
 import { DocumentService } from 'src/_services/document.service';
-import { LookUp } from 'src/_models/interfaces';
+import { User } from 'src/_models/interfaces';
 
 @Component({
   selector: 'app-document-upload',
@@ -20,33 +18,28 @@ export class DocumentUploadComponent implements OnInit {
 
   form!: FormGroup;
   isSubmitting: boolean = false;
-  selectedAuthors: any[] = [];
-  selectedDocument: File | undefined;
 
   // options for select menus
   document_types: any[] = [];
-  author_choices!: any;
+  author_choices!: User[] | any;
+
+  private formChangesSubscription!: Subscription;
 
   constructor(
     private formBuilder: FormBuilder,
-    private backendService: BackendService,
     private userService: UserService,
     private documentService: DocumentService,
-    private alertService: AlertService,
-    private authService: AuthService,
     private activeModal: NgbActiveModal,
   ) { }
 
   ngOnInit(): void {
     this.documentService.getDocumentTypes().subscribe(document_types => {
-      let options: { key: any, value: any }[] = []; // should be strings, but did any to get rid of error.
-      for (let type of document_types) {
-        options.push({ key: type.id, value: type.name })
-      }
-      this.document_types = options;
+      this.document_types = document_types.map(type => ({ key: type.id, value: type.name }));
     })
 
-    this.author_choices = this.buildEmployeeOptions();
+    this.userService.getUsers().subscribe(users => {
+      this.author_choices = users;
+    });
 
     this.form = this.formBuilder.group({
       title: ['', Validators.required],
@@ -56,72 +49,58 @@ export class DocumentUploadComponent implements OnInit {
       publish_date: ['', Validators.required],
       document_type: ['', Validators.required],
       citation: [''],
-      keywords: ['']
+      keywords: [''],
+      document: [null, Validators.required]
     })
+
+    // subscribe to form changes
+    this.form.valueChanges.subscribe(() => {
+      console.log('Form Valid:', this.form.valid);
+      console.log('Form Value:', this.form.value);
+    });
   }
 
   get f() { return this.form.controls; }
 
-  onFileChange(selectedFile: File | undefined): void {
-    this.selectedDocument = selectedFile;
-  }
-
   onSubmit(): void {
-    if (this.form.invalid || !this.selectedDocument) {
+    if (this.form.invalid) {
       return;
     }
 
     this.isSubmitting = true;
 
-    const formData = new FormData();
-    formData.append('title', this.f['title'].value);
-    formData.append('description', this.f['description'].value);
-    formData.append('primary_author', this.f['primary_author'].value);
+    // const formData = new FormData();
+    // formData.append('title', this.f['title'].value);
+    // formData.append('description', this.f['description'].value);
+    // formData.append('primary_author', this.f['primary_author'].value);
+    // const chosenAuthors = this.f['employee_authors'].value;
+    // chosenAuthors.forEach((author: any) => formData.append('employee_authors', author));
+    // formData.append('publish_date', this.f['publish_date'].value);
+    // formData.append('document_type', this.f['document_type'].value);
+    // formData.append('citation', this.f['citation'].value);
+    // formData.append('keywords', this.f['keywords'].value);
+    // formData.append('document', this.f['document'].value);
 
-    // formData.append('employee_authors', this.f['employee_authors'].value);
-    const chosenAuthors = this.f['employee_authors'].value;
-    for (let i = 0; i < chosenAuthors.length; i++) {
-      formData.append(`employee_authors`, chosenAuthors[i]);
+    // this.backendService.newItem('documents', formData).subscribe({
+    //   next: () => {
+    //     this.documentService.refreshDocuments().subscribe((updatedList: any) => {
+    //       this.context.data = updatedList;
+    //       this.authService.refreshPermissions().subscribe();
+    //       this.activeModal.close('success');
+    //     });
+    //   },
+    //   error: (err) => {
+    //     this.alertService.error('Failed to upload document.', { autoClose: true })
+    //     console.log(err);
+    //     this.isSubmitting = false;
+    //   }
+    // });
+  }
+
+  ngOnDestroy(): void {
+    if (this.formChangesSubscription) {
+      this.formChangesSubscription.unsubscribe();
     }
-
-    formData.append('publish_date', this.f['publish_date'].value);
-    formData.append('document_type', this.f['document_type'].value);
-    formData.append('citation', this.f['citation'].value);
-    formData.append('keywords', this.f['keywords'].value);
-    formData.append('document', this.selectedDocument);
-
-    this.backendService.newItem('documents', formData).subscribe({
-      next: () => {
-        this.documentService.refreshDocuments().subscribe((updatedList: any) => {
-          this.context.data = updatedList;
-          this.authService.refreshPermissions().subscribe();
-          this.activeModal.close('success');
-        });
-      },
-      error: (err) => {
-        this.alertService.error('Failed to upload document.', { autoClose: true })
-        console.log(err);
-        this.isSubmitting = false;
-      }
-    });
-  }
-
-  // for employees
-  private buildEmployeeOptions() {
-    let options: { key: string, value: string }[] = [];
-
-    this.userService.getUsers().subscribe((employees: any) => {
-      for (let emp of employees) {
-        options.push({ key: emp.id, value: emp.full_name })
-      }
-    });
-
-    return options;
-  }
-
-  updateAuthors() {
-    console.log('updating selectedAuthors to:', this.form.get(`employee_authors`));
-    this.form.get(`employee_authors`)?.patchValue(this.selectedAuthors);
   }
 
   @HostListener('window:popstate', ['$event'])
--- src/app/forms/file-input/file-input.component.ts
+++ src/app/forms/file-input/file-input.component.ts
@@ -1,22 +1,57 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-file-input',
   templateUrl: './file-input.component.html'
 })
-export class FileInputComponent {
-  @Input() validationErrorMessage: string = 'A file must be selected.';
-  @Output() fileSelected = new EventEmitter<File | undefined>();
+export class FileInputComponent implements OnInit {
+  @Input() form!: FormGroup;
+  @Input() controlName!: string;
+  @Input() maxFileSizeMB?: number; // Maximum allowed file size in MB
+  @Input() customErrors: { [key: string]: string } = {};
 
-  selectedFile: File | undefined;
-
-  onFileChange(event: any): void {
-    if (event.target.files && event.target.files.length > 0) {
-      this.selectedFile = event.target.files[0];
-      this.fileSelected.emit(this.selectedFile);
-    } else {
-      this.selectedFile = undefined;
-      this.fileSelected.emit(undefined);
-    }
+  get control(): FormControl {
+    return this.form.get(this.controlName) as FormControl;
   }
+
+  get isInvalid(): boolean {
+    return this.control.touched && this.control.dirty && this.control.invalid;
+  }
+
+  get errorMessages(): string[] {
+    const errors = this.control.errors || {};
+    return Object.keys(errors).map(key => this.customErrors[key] || errors[key]);
+  }
+
+  get id(): string {
+    return `file-input-${this.controlName}`;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onFileChange(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+
+      // Check for file size
+      if (this.maxFileSizeMB && file.size > this.maxFileSizeMB * 1024 * 1024) {
+        this.control.setErrors({ fileSize: `File size exceeds ${this.maxFileSizeMB} MB limit.` });
+      } else {
+        this.control.setErrors(null);
+        this.control.setValue(file);
+      }
+    } else {
+      this.control.setValue(null);
+      this.control.setErrors({ required: 'A file must be selected.' });
+    }
+
+    this.control.markAsTouched();
+    this.control.markAsDirty();
+  }
+
 }
