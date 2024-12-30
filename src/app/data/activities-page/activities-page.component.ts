5e85931f2c6b1e6f97ea48e519022f0f01f9422e
Tyler Stright <tylers@nezperce.org>
2024-12-30 11:19:38 -0700

    updated activities page to jive with updated serializer

--- src/app/data/activities-page/activities-page.component.ts
+++ src/app/data/activities-page/activities-page.component.ts
@@ -20,8 +20,8 @@ export class ActivitiesPageComponent {
   private gridApi!: GridApi;
   columnDefs: ColDef[] = [
     {headerName: 'Id', field: 'id', minWidth: 75, maxWidth: 75, resizable: false },
-    {headerName: 'Task Type', field: 'task.task_type.name' },
-    {headerName: 'Project', field: 'task.project.name' },
+    {headerName: 'Task Type', field: 'task_type_name' },
+    {headerName: 'Project', field: 'project_name' },
     {headerName: 'Last Updated', field: 'updated_at', minWidth: 150, maxWidth: 150, resizable: false },
     {headerName: 'Data', field: 'data', hide: true },
     {headerName: 'View', field: 'activity_id', cellRenderer: LinkButtonRendererComponent, cellRendererParams: {} },
